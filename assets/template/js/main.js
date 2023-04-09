f754855394e6996ed345ee17c7eb3036946d0d7b
lockican <44090215+lockican@users.noreply.github.com>
2023-04-10 02:09:17 +0300

    2

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -3,10 +3,5 @@ $( document ).ready(function() {
         $(this).toggleClass('open');
         $('.header').toggleClass('header--opened'); 
     })
-
-    $('.introduction-slider').slick({
-        infinite: false,
-        slidesToShow: 4,
-        swipeToSlide: true
-      });
+ 
 });
