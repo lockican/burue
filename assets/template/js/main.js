df09655b848c7e9e6dcd68df4f258810170eec62
lockican <44090215+lockican@users.noreply.github.com>
2023-04-10 18:02:16 +0300

    inner

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -3,5 +3,32 @@ $( document ).ready(function() {
         $(this).toggleClass('open');
         $('.header').toggleClass('header--opened'); 
     })
- 
+    
+    $('.realization_btn').click(function(){
+        if($(this).closest('.realization_tab').hasClass('active')){
+       
+            $(this).closest('.realization_tab').removeClass('active')
+            return
+        }
+
+        $('.realization_tab').removeClass('active')
+        $(this).closest('.realization_tab').addClass('active')
+       
+    })
+
+  
+    var swiper = new Swiper(".mySwiper", {
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 });
