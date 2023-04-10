6649ef22922d9b6e22e81645f77e2f54261aacbc
lockican <44090215+lockican@users.noreply.github.com>
2023-04-11 01:53:27 +0300

    4

---
+++ main.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+    $('.menu-btn').click(function(){
+        $(this).toggleClass('open');
+        $('.header').toggleClass('header--opened'); 
+    })
+    
+    $('.realization_btn').click(function(){
+        if($(this).closest('.realization_tab').hasClass('active')){
+       
+            $(this).closest('.realization_tab').removeClass('active')
+            return
+        }
+
+        $('.realization_tab').removeClass('active')
+        $(this).closest('.realization_tab').addClass('active')
+       
+    })
+
+  
+    var swiper = new Swiper(".mySwiper", {
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
+      var swiper2 = new Swiper(".mySwiper2", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+});
